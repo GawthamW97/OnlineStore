e169556cf281611c580ddbd5c9b82176960a3267
gawthamwayne97@gmail.com <39323106+GawthamW97@users.noreply.github.com>
2020-11-14 08:22:19 +0530

    Initial Commit
    Signed-off-by: gawthamwayne97@gmail.com <39323106+GawthamW97@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ProductsPage from "./components/ProductsPage";
+import ShoppingCart from "./components/ShoppingCart";
+
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "./App.css";
+import { Container } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Container>
+          <Route exact path="/" component={ProductsPage} />
+          <section>
+            <Switch>
+              <Route exact path="/cart" component={ShoppingCart} />
+            </Switch>
+          </section>
+        </Container>
+      </Router>
+      ;
+    </Provider>
   );
 }
 
---
+++ src/components/SingleProduct.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SingleProduct() {
+  return (
+    <div>
+      <h2>Single Product</h2>
+    </div>
+  );
+}
+
+export default SingleProduct;
